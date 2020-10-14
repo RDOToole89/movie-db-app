948a90d05f5f04defa523d88a0dacde4c98f11e3
RDOToole89 <roibinotoole@gmail.com>
2020-10-14 13:55:36 +0200

    add global css styles and navbar component styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
+import AboutPage from "./pages/DiscoverMoviesPage";
+import discoverMoviesPage from "./pages/DiscoverMoviesPage";
+import HomePage from "./pages/HomePage";
+import NavBar from "./components/NavBar/NavBar";
+import "./styles/App.css";
 
 function App() {
   return (
     <Router>
       <div className="App">
         Hello World!
-        {/* <Switch>
+        <NavBar />
+        <Switch>
           <Route path="/discover" component={discoverMoviesPage} />
           <Route path="/about" component={AboutPage} />
           <Route path="/" component={HomePage} />
-        </Switch> */}
+        </Switch>
       </div>
     </Router>
   );
